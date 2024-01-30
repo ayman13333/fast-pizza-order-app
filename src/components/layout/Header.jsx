38857a5b0910149b7c56e1f9b93bb4c1f3f16828
Ayman Mohamed <93885334+ayman13333@users.noreply.github.com>
2024-01-30 11:50:56 +0200

    update

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,13 +4,19 @@ export default function Header() {
     return (
 
         <header className="flex items-center justify-between">
-            <Link className="text-primary font-semibold text-2xl" href="">ST PIZZA</Link>
+            
             <nav className="flex items-center gap-8 text-gray-500 font-semibold">
+            <Link className="text-primary font-semibold text-2xl" href="">ST PIZZA</Link>
                 <Link href={''}>Home</Link>
                 <Link href={''}>Menu</Link>
                 <Link href={''}>About</Link>
                 <Link href={''}>Contact</Link>
-                <Link href={''} className="bg-primary text-white rounded-full px-8 py-2">Login</Link>
+                
+            </nav>
+
+            <nav className="flex items-center gap-8 text-gray-500 font-semibold">
+            <Link href={''} className="">Login</Link>
+            <Link href={''} className="bg-primary text-white rounded-full px-8 py-2">Register</Link>
             </nav>
         </header>
 
