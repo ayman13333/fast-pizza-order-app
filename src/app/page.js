97dff47f376e234a03077870eeb8b87f5d77b472
Ayman Mohamed <93885334+ayman13333@users.noreply.github.com>
2024-01-30 11:28:55 +0200

    update

--- src/app/page.js
+++ src/app/page.js
@@ -17,6 +17,14 @@ export default function Home() {
     <p className="max-w-2xl mx-auto mt-4 text-gray-500">
     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
     </p>
+
+    <section className="text-center mt-10">
+      <SectionHeaders title1={'Contact Us'} title2={'Dont hesitate'} />
+      <div className="mt-8">
+
+      <a className="text-4xl" href="tel:12345">12345</a>
+      </div>
+    </section>
     </>
   );
 }
