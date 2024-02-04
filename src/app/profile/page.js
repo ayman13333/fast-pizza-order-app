d3ed788881d9a71aaa05836023ed60e9a6a7e590
Ayman Mohamed <93885334+ayman13333@users.noreply.github.com>
2024-02-04 22:08:25 +0200

    update

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -2,33 +2,47 @@
 import { useSession } from "next-auth/react"
 import Image from "next/image";
 import { redirect } from "next/navigation";
+import { useState } from "react";
 
 export default function ProfilePage() {
     const session=useSession();
+    const[userName,setUserName]=useState(session?.data?.user?.name || '');
     const{status}=session;
 
     if(status==='loading') return 'Loading....';
     if(status==='unauthenticated') return redirect('/login');
     const userImage=session?.data?.user?.image;
 
+    async function handleProfileInfoUpdate(e) {
+        e.preventDefault();
+        await fetch('/api/profile',{
+            method:'PUT',
+            headers:{'Content-Type':'application/json'},
+            body:JSON.stringify({name:userName})
+        });
+    }
+
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Profile</h1>
-      <form className="max-w-md mx-auto items-center">
+      <div className="max-w-md mx-auto items-center">
         <div className="flex gap-4">
         <div>
             <div className="bg-gray-300 p-2 rounded-lg">
-            <Image src={userImage} width={80} height={80} alt="avatar" />
+            <Image className="rounded-lg w-full h-full mb-1" src={userImage} width={250} height={250} alt="avatar" />
             <button type="button">Change avatar</button>
-            </div>
-            
+            </div>   
         </div>
-        <div className="grow">
-        <input type="text" placeholder="first and last name" />
+        <form className="grow" onSubmit={handleProfileInfoUpdate}>
+        <input type="text" 
+        value={userName}
+        onChange={e=>setUserName(e.target.value)}
+        placeholder="first and last name" />
+        <input type="email" disabled={true} value={session?.data?.user?.email} />
         <button type="submit">Save</button>
+        </form>
         </div>
-        </div>
-      </form>
+      </div>
     </section>
   )
 }
