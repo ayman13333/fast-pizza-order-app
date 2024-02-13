c90297d08aca2c1ca2be245c36bc9179791a704d
Ayman Mohamed <93885334+ayman13333@users.noreply.github.com>
2024-02-13 23:52:29 +0200

    update

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -1,3 +1,17 @@
-export function name() {
-    
+import { Category } from "../../../models/Category";
+const { default: mongoose } = require("mongoose");
+mongoose.connect(process.env.MONGO_URL);
+
+export async function POST(req) {
+   // console.log('xxxxxxxxxxxxxx');   
+    const {name}=await req.json();
+    const newCategory=await Category.create({name});
+
+    return Response.json(newCategory);
+}
+
+export async function GET(){
+    return Response.json(
+        await Category.find()
+    );
 }
--- src/app/categories/page.jsx
+++ src/app/categories/page.jsx
@@ -1,9 +1,59 @@
+"use client";
 import Tabs from "@/components/layout/Tabs";
+import { useEffect, useState } from "react";
 
-export default function page() {
+export default function Page() {
+  const[categories,setCategories]=useState([]);
+  const[newCategoryName,setNewCategoryName]=useState('');
+  const[editedCategory,setEditedCategory]=useState(null);
+
+
+  useEffect(()=>{
+    fetch('/api/categories').then(res=>res.json())
+    .then(categoriesRes=>setCategories(categoriesRes));
+  },[]);
+
+  async function handleAddCategory(e) {
+    e.preventDefault();
+   let response= await fetch('/api/categories',{
+      method:'POST',
+      headers:{'Content-Type':'application/json'},
+      body:JSON.stringify({name:newCategoryName})
+    });
+    response=await response.json();
+    //console.log(response);
+    if(response._id) setCategories(prev=>[...prev,response]);
+    setNewCategoryName('');
+  }
   return (
     <section className="mt-8 max-w-lg mx-auto">
       <Tabs isAdmin={true} />
+      <form className="mt-8" onSubmit={handleAddCategory}>
+      <div className="flex gap-2 items-end">
+      <div className="grow">
+      <label>New category name</label>
+      <input value={newCategoryName} onChange={(e)=>setNewCategoryName(e.target.value)} type="text" />
+      </div>
+      <div className="pb-2">
+      <button type="submit">
+       {editedCategory ? 'Update' : 'Create'} 
+        </button>
+      </div>
+      </div>
+      </form>
+      <h2 className="mt-8 text-sm text-gray-500">Edit Category:</h2>
+      {categories?.length>0 && categories.map(c=>(
+        <button 
+        className="bg-gray-200 rounded-lg p-2 px-4 flex gap-1 cursor-pointer my-1"
+        key={c._id}
+        onClick={()=>{
+          setEditedCategory(c);
+          setNewCategoryName(c.name);
+        }}
+        >
+        {c.name}
+        </button>
+      ))}
     </section>
   )
 }
