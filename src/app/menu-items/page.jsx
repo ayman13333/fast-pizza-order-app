bfc80ede50dd1cb02d9d65cf61c9afe89589e774
Ayman Mohamed <93885334+ayman13333@users.noreply.github.com>
2024-02-15 12:45:42 +0200

    update

---
+++ src/app/menu-items/page.jsx
@@ -0,0 +1,10 @@
+import Tabs from "@/components/layout/Tabs";
+
+export default function page() {
+  return (
+    <section className="mt-8">
+      <Tabs isAdmin={true} />
+      menuItems
+      </section>
+  )
+}
