7fd492ece491632684b1b447ddd20da6a7938d3f
Ayman Mohamed <93885334+ayman13333@users.noreply.github.com>
2024-02-15 14:57:59 +0200

    update

--- src/app/menu-items/page.jsx
+++ src/app/menu-items/page.jsx
@@ -1,10 +1,35 @@
+'use client';
 import Tabs from "@/components/layout/Tabs";
+import { useState } from "react";
 
-export default function page() {
+export default function Page() {
+  const[name,setName]=useState('');
+  const[desc,setDesc]=useState('');
+  const[price,setPrice]=useState(0);
+
+  async function handleFormSubmit(e){
+    e.preventDefault();
+
+  }
   return (
     <section className="mt-8">
       <Tabs isAdmin={true} />
-      menuItems
+      <form onSubmit={handleFormSubmit} className="mt-8">
+      <div className="flex gap-2 mx-auto max-w-md items-end">
+      <div className="grow">
+        <label>Menu item name</label>
+        <input value={name} onChange={(e)=>setName(e.target.value)} type="text" />
+
+        <label>Description</label>
+        <input value={desc} onChange={(e)=>setDesc(e.target.value)} type="text" />
+
+        <label>Base price</label>
+        <input value={price} onChange={(e)=>setPrice(e.target.value)} type="number" />
+
+        <button type="submit">Create</button>
+      </div>
+      </div>
+      </form>
       </section>
   )
 }
