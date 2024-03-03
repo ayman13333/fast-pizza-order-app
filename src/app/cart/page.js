2c12e88fbd940f30f2890b0d4544f8b2bef32714
Ayman Mohamed <93885334+ayman13333@users.noreply.github.com>
2024-03-03 20:39:16 +0200

    update

---
+++ src/app/cart/page.js
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Page() {
+  return (
+    <section className="mt-8">
+      cart
+    </section>
+  )
+}
--- src/components/Menu/MenuItem.jsx
+++ src/components/Menu/MenuItem.jsx
@@ -9,20 +9,49 @@ export default function MenuItem(MenuItem) {
   const { name, description, basePrice, sizes, extras } = MenuItem;
   const { addToCart } = useContext(CartContext);
   const [showPoPUP, setShowPoPUP] = useState(false);
+  const[selectedSize,setSelectedSize]=useState(null);
+  const[selectedExtras,setSelectedExtras]=useState([]);
 
+  
   function handleAddToCart() {
     // ()=>addToCart(MenuItem)
+    if(showPoPUP){
+      addToCart(MenuItem,selectedSize,selectedExtras);
+      setShowPoPUP(false);
+      return;
+    }
     if (sizes.length == 0 && extras.length == 0) addToCart(MenuItem)
     else setShowPoPUP(true);
+  }
+
+  function handleExtrasClick(e,extra) {
+    const checked=e.target.checked;
+    if(checked) setSelectedExtras(prev=>[...prev,extra]);
+    else setSelectedExtras(prev=>prev.filter(e=>e.name!==extra.name));
+    
+  }
+
+  let selectedPrice=basePrice;
+  if(selectedSize) selectedPrice+=selectedSize.price;
+
+  if(selectedExtras?.length>0){
+    for(const extra of selectedExtras)
+    {
+      selectedPrice+=extra.price;
+    }
   }
 
   return (
     <>
       {
         showPoPUP&&(
-          <div className="fixed inset-0 bg-black/80 flex items-center justify-center">
-            <div className="bg-white p-4 rounded-lg max-w-md">
-              
+          <div 
+          onClick={()=>setShowPoPUP(false)}
+          className="fixed inset-0 bg-black/80 flex items-center justify-center">
+            <div 
+            onClick={e=>e.stopPropagation()}
+            className="bg-white p-4 rounded-lg max-w-md overflow-y-scroll">
+
             <img src="/pizza.png" className="max-h-24 mx-auto block" alt="pizza" />
 
             <h2 className="text-lg font-bold text-center mb-2">{name}</h2>
@@ -35,7 +64,7 @@ export default function MenuItem(MenuItem) {
                   <label 
                   className="block p-4 mb-2 rounded-md border text-start"
                   key={size}>
-                    <input type="radio" name="size" />{size.name} ${basePrice+ size.price}
+                    <input type="radio" name="size" onClick={()=>setSelectedSize(size)} checked={selectedSize?.name===size.name} />{size.name} ${basePrice+ size.price}
                   </label>
                 ))
 
@@ -43,21 +72,22 @@ export default function MenuItem(MenuItem) {
               </div>
             )}
 
-            {
-              extras?.length>0&&(
+            {extras?.length>0&&(
                 <div className="p-2">
                 <h3 className="mb-2 text-gray-700">Pick your extras:</h3>
                 {extras.map(extras=>(
                   <label 
                   className="block p-4 mb-2 rounded-md border text-start"
                   key={extras}>
-                    <input type="checkbox" name={extras.name} />{extras.name} ${basePrice+ extras.price}
+                    <input type="checkbox" onClick={(e)=>handleExtrasClick(e,extras)} name={extras.name} />{extras.name} ${basePrice+ extras.price}
                   </label>
                 ))
                 }
               </div>
               )
             }
+
+            <button onClick={handleAddToCart} className="primary" type="button">Add to Cart {selectedPrice}$ </button>
             </div>
           </div>
         )
